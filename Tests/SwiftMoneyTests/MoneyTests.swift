9300c28c129bb866b2a9a872cdf6c1110e2ae247
Paul Willis <ios_dev@paulrobertwillis.com>
2025-10-08 14:41:48 +0100

    add tests for decimal addition

--- Tests/SwiftMoneyTests/MoneyTests.swift
+++ Tests/SwiftMoneyTests/MoneyTests.swift
@@ -14,12 +14,25 @@ struct MoneyTests {
     }
     
     @Test
-    func canAddTwoMoneysOfSameCurrency() {
+    func canAddMoneysOfSameCurrency() {
         let a = Money<TST>(decimalValue: 2)
         let b = Money<TST>(decimalValue: 3)
-        let expected = Money<TST>(decimalValue: 5)
+        let c = Money<TST>(decimalValue: 5)
+        let expected = Money<TST>(decimalValue: 10)
         
-        let actual = a + b
+        let actual = a + b + c
+        
+        #expect(actual == expected)
+    }
+    
+    @Test
+    func canAddDecimalMoneysOfSameCurrency() {
+        let a = Money<TST>(decimalValue: 0.1)
+        let b = Money<TST>(decimalValue: 0.1)
+        let c = Money<TST>(decimalValue: 0.1)
+        let expected = Money<TST>(decimalValue: 0.3)
+        
+        let actual = a + b + c
         
         #expect(actual == expected)
     }
