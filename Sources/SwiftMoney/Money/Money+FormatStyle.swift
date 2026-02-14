1d2b0641278cf6d276736cc309977bb94e4cc3ca
Paul Willis <ios_dev@paulrobertwillis.com>
2026-02-14 10:34:40 +0000

    docs(Money): add notes for development of feature

--- Sources/SwiftMoney/Money/Money+FormatStyle.swift
+++ Sources/SwiftMoney/Money/Money+FormatStyle.swift
@@ -23,6 +23,18 @@ extension Money {
             self.signDisplayStrategy = signDisplayStrategy
         }
 
+//        public var attributed: FormatStyle.Attributed {
+//
+//        }
+//
+//        public func grouping(_ group: Configuration.Grouping) -> FormatStyle {
+//
+//        }
+//
+//        public func precision(_ p: Configuration.Precision) -> FormatStyle {
+//
+//        }
+
         public func sign(
             strategy: Configuration.SignDisplayStrategy
         ) -> FormatStyle {
@@ -31,9 +43,69 @@ extension Money {
                 signDisplayStrategy: strategy
             )
         }
+
+//        public func decimalSeparator(
+//            strategy: Configuration.DecimalSeparatorDisplayStrategy
+//        ) -> FormatStyle {
+//
+//        }
+//
+//        public func rounded(
+//            rule: Configuration.RoundingRule = .toNearestOrEven,
+//            increment: Int? = nil
+//        ) -> FormatStyle {
+//
+//        }
+//
+//        public func scale(_ multiplicand: Double) -> FormatStyle {
+//
+//        }
+//
+//        public func presentation(
+//            _ p: Configuration.Presentation
+//        ) -> FormatStyle {
+//
+//        }
+//
+//        /// Modifies the format style to use the specified notation.
+//        ///
+//        /// - Parameter notation: The notation to apply to the format style.
+//        /// - Returns: An integer currency format style modified to use the specified notation.
+//        @available(macOS 15, iOS 18, tvOS 18, watchOS 11, *)
+//        public func notation(
+//            _ notation: Configuration.Notation
+//        ) -> FormatStyle {
+//
+//        }
     }
 }
 
+
+/*
+
+ attributed
+
+ decimalSeparator
+
+ grouping
+
+ notation
+
+ precision
+
+ presentation
+
+ rounded
+
+ scale
+
+ sign
+
+ currencyCode
+
+ */
+
+#warning("This availability needs replacing")
 @available(macOS 12.0, *)
 extension Money.FormatStyle: Foundation.FormatStyle {
     public func format(_ value: Money) -> String {
@@ -49,8 +121,32 @@ extension Money.FormatStyle: Foundation.FormatStyle {
 
 #warning("`.presentation(.narrow) will need to be modifiable. Dollars are automatically marked US$ by default, but this would likely be undesirable for US users. Using `.narrow` means different dollar currencies can be confused with each other.")
 
+#warning("Add tests to ensure this works as expected")
 extension Money {
+    /// Format `self` using `Money.FormatStyle()`
     public func formatted() -> String {
         Self.FormatStyle().format(self)
     }
 }
+
+//extension Money {
+//    /// Format `self` with the given format.
+//    public func formatted<S: Foundation.FormatStyle>(
+//        _ format: S
+//    ) -> S.FormatOutput
+//    where S.FormatInput == Money {
+//        format.format(self)
+//    }
+//}
+//
+
+//extension FormatStyle {
+//    public static func sign<C: Currency>() -> Self
+//    where Self == Money<C>.FormatStyle {
+//        let formatStyle = Money<C>.FormatStyle()
+//
+//        formatStyle.sign
+//
+//        return formatStyle
+//    }
+//}
--- Sources/SwiftMoney/Money/Money.swift
+++ Sources/SwiftMoney/Money/Money.swift
@@ -41,6 +41,7 @@ extension Money: CustomStringConvertible {
     }
 }
 
+#warning("Add tests to ensure these are conformed to as expected")
 extension Money: Equatable {}
 
 extension Money: Hashable {}
@@ -48,3 +49,86 @@ extension Money: Hashable {}
 extension Money: Sendable {}
 
 extension Money: Codable {}
+
+
+
+
+/*
+
+    - `.zero`
+    - `.magnitude`
+
+    - formatting (CurrencyFormatStyle from IntegerFormatStyle)
+
+    - addition
+        > associative: a + (b + c) == (a + b) + c
+        > commutative: a + b == b + a
+
+    - multiplication
+        > integeral
+        > fractional
+        > distributive: a x (b + c) == (a x b) + (a x c)
+
+    - distribution
+        > integral (chunks)
+        > `Distribution` type (`Distribution<Money>`?)
+
+    - formatting
+        > parsing using formatting options
+        > attributed
+        > decimalSeparator
+        > grouping
+        > notation
+        > precision
+        > presentation
+        > rounded
+        > scale
+        > sign
+        > currencyCode
+        >`precision` and `notation`, etc., with comprehensive documentation for all formatting options
+            (see bookmarks for Swift Forums discussion on compact notation)
+        > Attributed as a FormatStyle
+        > Add `Money.Currency` as in `IntegerFormatStyle.Currency`?
+
+
+    - non-decimal currencies
+
+    - serialisation
+        > custom encoding + decoding
+
+    - conformance to `DebugCustomStringConvertible`
+
+    - additional type-safe values
+        > `PositiveMoney` typealias `Credit`?
+        > `NegativeMoney` typealias `Debit`?
+        > `ZeroMoney`
+        > `NonPositiveMoney`
+        > `NonNegativeMoney`
+
+    - handling non-decimalised currencies (which ones exist?)
+
+    - handling "transaction minor units" vs. "tender minor units" as in Swiss Francs (CHF) of 0.01 vs. 0.05
+
+    - init from Decimal?
+        > Potentially by multiplying by minor units?
+        > How handle small values like £0.003 (3/10ths pence)?
+
+    - init from String (optional)
+    - init from minor units (non optional)?
+
+    - poison addition, subtraction, and multiplication operators for Double and Float.
+    - additionally poison division operators for all number types: Int, Decimal, Double, Float (Int128? UInts? Binary ...?)
+
+    - comparators (<, >, etc.)
+
+    - negating prefix (-)
+
+    - conformance to `AdditiveArithmetic`?
+
+    * Minimal quantisation
+    - When choosing 0.0001 USD as the minimal quantisation of USD, this allows us to represent a quadrillion dollars. This is more than the current M1 money supply of USD.
+
+    - String intepolation? (Test separately to `.description`!)
+
+    - `intValue` and `decimalValue`
+ */
