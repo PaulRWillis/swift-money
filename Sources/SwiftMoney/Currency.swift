e747ca58fa477d5b9c19be394a0a0d6d2fc559c7
Paul Willis <ios_dev@paulrobertwillis.com>
2026-04-02 21:05:41 +0100

    refactor(Currency): constrain `minorUnits` from `Int` to `Int64`

--- Sources/SwiftMoney/Currency.swift
+++ Sources/SwiftMoney/Currency.swift
@@ -2,30 +2,30 @@ import Foundation
 
 public protocol Currency: Equatable, Hashable, Sendable {
     static var code: String { get }
-    static var minorUnits: Int {  get }
+    static var minorUnits: Int64 {  get }
 }
 
 public enum EUR: Currency {
     public static let code: String = "EUR"
-    public static let minorUnits: Int = 100
+    public static let minorUnits: Int64 = 100
 }
 
 public enum GBP: Currency {
     public static let code: String = "GBP"
-    public static let minorUnits: Int = 100
+    public static let minorUnits: Int64 = 100
 }
 
 public enum USD: Currency {
     public static let code: String = "USD"
-    public static let minorUnits: Int = 100
+    public static let minorUnits: Int64 = 100
 }
 
 public enum JPY: Currency {
     public static let code: String = "JPY"
-    public static let minorUnits: Int = 1
+    public static let minorUnits: Int64 = 1
 }
 
 public enum CHF: Currency {
     public static let code: String = "CHF"
-    public static let minorUnits: Int = 100
+    public static let minorUnits: Int64 = 100
 }
--- Tests/SwiftMoneyTests/TestCurrency.swift
+++ Tests/SwiftMoneyTests/TestCurrency.swift
@@ -2,5 +2,10 @@ import SwiftMoney
 
 enum TST: Currency {
     static let code: String = "TST"
-    static let minorUnits: Int = 100
+    static let minorUnits: Int64 = 100
+}
+
+enum NO_MINOR_UNITS: Currency {
+    static let code: String = "NO_MINOR_UNITS"
+    static let minorUnits: Int64 = 0
 }
