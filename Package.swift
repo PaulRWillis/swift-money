d99d3a8a6fd2af167d7908275b9298424d04cd1c
Paul Willis <ios_dev@paulrobertwillis.com>
2026-04-22 17:49:32 +0100

    chore(SwiftMoney): Add support for multiple platforms, including iOS and watchOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,11 @@ import PackageDescription
 let package = Package(
     name: "SwiftMoney",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v15),
+        .iOS(.v18),
+        .watchOS(.v11),
+        .tvOS(.v18),
+        .visionOS(.v2),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
