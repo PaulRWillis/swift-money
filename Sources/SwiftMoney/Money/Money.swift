69e751bd64dadd986d2b36f21a2b83ef037bd941
Paul Willis <ios_dev@paulrobertwillis.com>
2026-06-08 16:11:51 +0100

    refactor(Money): Move `Money+Numeric` code to main file

--- Sources/SwiftMoney/Money/Money.swift
+++ Sources/SwiftMoney/Money/Money.swift
@@ -368,6 +368,57 @@ extension Money: Hashable {
     }
 }
 
+// MARK: - Magnitude
+
+extension Money {
+    /// The magnitude type.
+    public typealias Magnitude = Money
+
+    /// The absolute value of this instance.
+    ///
+    /// Returns the non-negative value. Traps if the value is not representable (e.g. minimum value).
+    ///
+    /// ```swift
+    /// let v = Money("-5.0")!
+    /// v.magnitude  // 5.0
+    /// ```
+    public var magnitude: Magnitude {
+        _storage < .zero ? Self(_storage: -_storage) : self
+    }
+}
+
+// MARK: - Negation
+
+extension Money {
+    /// Returns the additive inverse of this value.
+    ///
+    /// ```swift
+    /// let price = Money<GBP>(4250) // £42.50
+    /// let neg = -price  // -£42.50
+    /// ```
+    ///
+    /// - Parameter operand: The value to negate.
+    /// - Returns: The negated value.
+    public prefix static func - (operand: Money) -> Money {
+        var copy = operand
+        copy.negate()
+        return copy
+    }
+
+    /// Replaces this value with its additive inverse.
+    ///
+    /// Traps if the value is not representable (e.g. minimum value).
+    ///
+    /// ```swift
+    /// var price = Money<GBP>(4250) // £42.50
+    /// price.negate()
+    /// // price is now -4250 (-£42.50)
+    /// ```
+    public mutating func negate() {
+        self = Self(_storage: -_storage)
+    }
+}
+
 #warning("Remove minimum/maximum")
 // MARK: - minimum / maximum
 
