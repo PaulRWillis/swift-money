2c90fe02a3f07ccd53eb2bf3252bc7560c57f862
Paul Willis <ios_dev@paulrobertwillis.com>
2026-06-08 16:18:51 +0100

    refactor(Money): Move `Money+Literals` code to main file

--- Sources/SwiftMoney/Money/Money+Literals.swift
+++
@@ -1,8 +0,0 @@
-// MARK: - ExpressibleByIntegerLiteral (poisoned)
-
-extension Money {
-    @available(*, unavailable, message: "Use Money(minorUnits:) for explicit minor-unit values")
-    public init(integerLiteral value: IntegerLiteralType) {
-        fatalError("Use Money(minorUnits:) for explicit minor-unit values")
-    }
-}
--- Sources/SwiftMoney/Money/Money.swift
+++ Sources/SwiftMoney/Money/Money.swift
@@ -806,6 +806,15 @@ extension UInt32 {
     }
 }
 
+// MARK: - ExpressibleByIntegerLiteral (poisoned)
+
+extension Money {
+    @available(*, unavailable, message: "Use Money(minorUnits:) for explicit minor-unit values")
+    public init(integerLiteral value: IntegerLiteralType) {
+        fatalError("Use Money(minorUnits:) for explicit minor-unit values")
+    }
+}
+
 #warning("Remove minimum/maximum")
 // MARK: - minimum / maximum
 
