5e1641912ec7163741ca8c8c8ada88c16475949a
Paul Willis <ios_dev@paulrobertwillis.com>
2026-06-19 13:31:49 +0100

    refactor(CurrencyCode): Rename local property from `string` to `value`

--- Sources/SwiftMoney/Currency/CurrencyCode/CurrencyCode.swift
+++ Sources/SwiftMoney/Currency/CurrencyCode/CurrencyCode.swift
@@ -82,9 +82,9 @@ extension CurrencyCode: Comparable {
 extension CurrencyCode: Codable {
     public init(from decoder: any Decoder) throws {
         let container = try decoder.singleValueContainer()
-        let string = try container.decode(String.self)
+        let value = try container.decode(String.self)
 
-        guard let s = Self.parse(string) else {
+        guard let s = Self.parse(value) else {
             throw DecodingError.dataCorruptedError(
                 in: container,
                 debugDescription: "CurrencyCode cannot be empty"
