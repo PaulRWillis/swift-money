cef891b4cf631d66856bd560b1ae9d1d6f6cbcf2
Paul Willis <ios_dev@paulrobertwillis.com>
2026-06-19 13:21:30 +0100

    refactor(MinimalQuantization): move `CustomStringConvertible` conformance to main file

--- Sources/SwiftMoney/Currency/MinimalQuantisation/MinimalQuantisation.swift
+++ Sources/SwiftMoney/Currency/MinimalQuantisation/MinimalQuantisation.swift
@@ -115,6 +115,13 @@ extension MinimalQuantisation: ExpressibleByIntegerLiteral {
     }
 }
 
+// MARK: - CustomStringConvertible
+
+extension MinimalQuantisation: CustomStringConvertible {
+    /// The quantisation as a decimal string, e.g. `"100"`.
+    public var description: String { String(_value) }
+}
+
 // MARK: - Int64 conversion
 
 extension Int64 {
