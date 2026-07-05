55e5446feea12035b65817f97dc8d8d670da3a52
Paul Willis <ios_dev@paulrobertwillis.com>
2026-07-05 09:04:14 +0100

    chore(SwiftMoneySerialization): add new module to package

--- Package.swift
+++ Package.swift
@@ -16,18 +16,21 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftMoney",
-            targets: ["SwiftMoney"]
+            targets: ["SwiftMoney"],
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftMoney"
+            name: "SwiftMoney",
+        ),
+        .target(
+            name: "SwiftMoneySerialization",
         ),
         .testTarget(
             name: "SwiftMoneyTests",
-            dependencies: ["SwiftMoney"]
+            dependencies: ["SwiftMoney"],
         ),
     ]
 )
